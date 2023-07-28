af295c61ceb2f4e278885bf4327a3bfe7c3615aa
unknown <edgarerb@hotmail.es>
2023-07-28 17:11:13 +0200

    full aplicacion

--- src/BuscadorPeliculas.jsx
+++ src/BuscadorPeliculas.jsx
@@ -1,6 +1,64 @@
+import { useState } from "react"
+
+
 
 export const BuscadorPeliculas = () => {
-  return (
-    <div>BuscadorPeliculas</div>
-  )
+
+    const urlBase = 'https://api.themoviedb.org/3/search/movie'
+    const API_KEY = 'db41d0835f044a73cb88dcda1347fb52'
+
+const [busqueda, setBusqueda] = useState('')
+
+const [peliculas, setPeliculas] = useState([])
+
+const handleInputChange = (e) => {
+    setBusqueda(e.target.value)
 }
+
+const handleSubmit = (e) => {
+    e.preventDefault()
+    fetchPeliculas()
+}
+
+const fetchPeliculas = async () => {
+    try {
+        const results = await fetch(`${urlBase}?query=${busqueda}&api_key=${API_KEY}`)
+        const data = await results.json()
+        setPeliculas(data.results)
+    } 
+    catch (error) {
+        console.error('ha ocurrido un error' , error)
+        
+    }
+}
+
+
+  return (
+    <div className="container">
+
+    <h1 className="title">Buscador de películas</h1>
+    <form onSubmit={handleSubmit}>
+        <input type="text"
+        placeholder="Escribe una película"
+        value={busqueda}
+        onChange={handleInputChange}/>
+    
+    <button type="submit" className="search-button">Buscar</button>
+    </form>
+    
+    <div className="movie-list">
+        {peliculas.map((pelicula) => (
+            <div key={pelicula.id} className="movie-card">
+                <img src={`https://image.tmdb.org/t/p/w500${pelicula.poster_path}`} alt={pelicula.title}/>
+                <h2>{pelicula.title}</h2>
+                <p>{pelicula.overview}</p>
+                 </div>
+        
+
+))}
+
+
+
+    </div>
+  
+</div>)}
